920644f233bc6a7d1f69b5e93b9ca28f9cf52a72
Feather <spacey2001@gmail.com>
2014-04-23 11:02:50 +0200

    First commits of web interface

---
+++ trunk/PongWeb/WebController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PongWeb
+{
+    public class WebController : Controller
+    {
+        //
+        // GET: /Index/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
